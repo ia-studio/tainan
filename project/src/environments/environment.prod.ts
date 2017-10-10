a1494763881bbfe3353ef9f6b156bff17c99775e
ilandy <w9874123@gmail.com>
2017-10-11 00:33:54 +0800

    首頁完成

---
+++ project/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  googleMapApiKey: 'AIzaSyC4jMQe13anvhR-iNWh3gMESeQK53hMYm0'
+};
---
+++ solution_01/src/app/shared/global.service.ts
@@ -0,0 +1,10 @@
+import { Injectable }     from '@angular/core';
+
+@Injectable()
+export class GlobalService {
+  public showPhoneButton = false;
+}
+export class TitleService {
+
+  index = '市長信箱 - 政府線上即時服務平台'
+}
